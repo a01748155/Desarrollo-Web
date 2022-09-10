fbaa3bffd1e4d676ba78a16eef719daf1d3b99c8
a01748155 <42364366+a01748155@users.noreply.github.com>
2022-09-10 14:37:17 -0500

    Add files via upload

---
+++ deploy/routes/consola.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const consolaController = require('../controllers/consola')//cuando se usa require no es necesario colocar .js en el archivo
+
+
+//Create Read Update Delete (CRUD) (ABCC) Altas Bajas Cambios Consultas
+
+//Servicio para mostrar el formulario
+router.get('/altaConsola',consolaController.getAltaConsola);
+//Servicio para procesar los datos del formulario
+router.post('/altaConsola',consolaController.postAltaConsola);
+//Servicio para consultar todos los datos
+router.get('/consultaConsola',consolaController.getConsultaConsola);
+
+module.exports = router;
